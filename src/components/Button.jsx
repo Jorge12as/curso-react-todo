391fa086f304ab9777b0d10c946a35f166480743
Jorge12as <jejsantosce@gmail.com>
2026-08-13 12:56:00 -0300

    fix: task completion icon

--- src/components/Button.jsx
+++ src/components/Button.jsx
@@ -1,12 +1,12 @@
 import PropTypes from "prop-types";
 
-function Button(props) {
+function Button(children, ...props) {
   return (
     <button
       {...props}
       className="bg-slate-500 p-2 rounded-md text-gray-200 hover:bg-slate-600"
     >
-      {props.children}
+      {children}
     </button>
   );
 }
