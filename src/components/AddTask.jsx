b591baebceea14df206b104be90387cf50586e44
Jorge12as <jejsantosce@gmail.com>
2026-08-12 21:07:59 -0300

    Inicial

--- src/components/AddTask.jsx
+++ src/components/AddTask.jsx
@@ -2,26 +2,22 @@ import { useState } from "react";
 import { v4 } from "uuid";
 import Input from "./input";
 
-function AddTask({onAddTaskSubmit}) {
-
+function AddTask({ onAddTaskSubmit }) {
   const [title, setTitle] = useState("");
   const [description, setDescription] = useState("");
-  
+
   return (
     <div className="space-y-4 p-6 bg-slate-400 rounded-md shadow flex flex-col">
-      <Input
-        type="text"
-        placeholder="Digite o titulo da tarefa"       
-        value={title}
+      <Input>
+        type="text" placeholder="Digite o titulo da tarefa" value={title}
         onChange={(Event) => setTitle(Event.target.value)}
-      />
+      </Input>
 
-      <Input
-        type="text"
-        placeholder="Digite a descrição da tarefa"
-        value={description}
+      <Input>
+        type="text" placeholder="Digite a descrição da tarefa" value=
+        {description}
         onChange={(Event) => setDescription(Event.target.value)}
-      />
+      </Input>
 
       <button
         className="text-white bg-slate-700 px-4 py-2 rounded-md font-medium"
@@ -37,8 +33,6 @@ function AddTask({onAddTaskSubmit}) {
       >
         Adicionar
       </button>
-
-      
     </div>
   );
 }
