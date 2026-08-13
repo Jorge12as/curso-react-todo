92af64990900fc340227e89dd155f71e3f7ee046
Jorge12as <jejsantosce@gmail.com>
2026-08-13 11:46:27 -0300

    Clean up AddTask component by removing comments
    Removed commented-out code for the AddTask component.

--- src/components/AddTask.jsx
+++ src/components/AddTask.jsx
@@ -46,47 +46,4 @@ AddTask.propTypes = {
 
 export default AddTask;
 
-// import { useState } from "react";
-// import Input from "./Input";
-// import PropTypes from "prop-types";
 
-// function AddTask({ onAddTaskSubmit }) {
-//   const [title, setTitle] = useState("");
-//   const [description, setDescription] = useState("");
-
-//   AddTask.propTypes = {
-//     onAddTaskSubmit: PropTypes.func.isRequired,
-//   };
-
-//   return (
-//     <div className="space-y-4 p-6 bg-slate-400 rounded-md shadow flex flex-col">
-//       <Input>
-//         type="text" placeholder="Digite o titulo da tarefa" value={title}
-//         onChange={(Event) => setTitle(Event.target.value)}
-//       </Input>
-
-//       <Input>
-//         type="text" placeholder="Digite a descrição da tarefa" value=
-//         {description}
-//         onChange={(Event) => setDescription(Event.target.value)}
-//       </Input>
-
-//       <button
-//         className="text-white bg-slate-700 px-4 py-2 rounded-md font-medium"
-//         onClick={() => {
-//           // verificar se o título e a descrição estão preenchidas
-//           if (!title.trim() || !description.trim()) {
-//             return alert("Preencha o título e a descrição da tarefa");
-//           }
-//           onAddTaskSubmit(title, description);
-//           setTitle("");
-//           setDescription("");
-//         }}
-//       >
-//         Adicionar
-//       </button>
-//     </div>
-//   );
-// }
-
-// export default AddTask;
